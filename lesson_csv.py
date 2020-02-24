96c933e61b07a175a40269c6e06b703a5928372d
ismirnov-v <ismirnov-v@outlook.com>
2020-02-24 21:50:36 +0300

    Обновил файлы

---
+++ lesson_csv.py
@@ -0,0 +1,16 @@
+import csv
+
+list_of_dict = [
+    {'name': 'Маша', 'age': 25, 'job': 'Scientist'},
+    {'name': 'Вася', 'age': 8, 'job': 'Programmer'},
+    {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+]
+
+
+with open('users.csv', 'w', encoding='utf-8', newline='') as f:
+    fields = ['name', 'age', 'job']
+    writer = csv.DictWriter(f, fields, delimiter=';')
+    writer.writeheader()
+    for user in list_of_dict:
+        writer.writerow(user)
+        
--- lesson_file.py
+++ lesson_file.py
@@ -15,12 +15,14 @@ with open('text.txt', 'r', encoding='utf-8') as f:
         line = line.upper()
         print(line)
 
-with open('referat.txt', 'r', encoding='utf-8') as f:
-    referat_conetnt = f.read()
-    
-print(f'Длина строки файла "реферат" равна: {len(referat_conetnt)}')
-print(f'Количество слов в файле составляет: {len(referat_conetnt.split())}')
 
-referat_new = referat_conetnt.replace(".", "!")
+with open('referat.txt', 'r', encoding='utf-8') as f:
+    referat_content = f.read()
+        
+    
+print(f'Длина строки файла "реферат" равна: {len(referat_content)}')
+print(f'Количество слов в файле составляет: {len(referat_content.split())}')
+
+referat_new = referat_content.replace(".", "!")
 with open('referat_new.txt', 'w', encoding='utf-8') as f:
     f.write(referat_new)
